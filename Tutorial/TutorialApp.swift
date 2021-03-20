3a848bf8825d653d08af46b70c22dd7012fd2d68
Shu Tanabe <sfcofc@gmail.com>
2021-03-20 12:03:18 +0900

    Initial Commit

---
+++ Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by 田邉舟 on 2021/03/20.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
